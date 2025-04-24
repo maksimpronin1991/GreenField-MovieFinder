5be92e1c89a622bc14fcaf688a85b19a414af527
MaksimPro <maksimpronin1991@icloud.com>
2025-04-24 14:35:43 +0300

    feat: login screen

---
+++ src/components/login-screen/loginScreen.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const LoginScreen: React.FC = () => {
+  return (
+    <div className="login-screen">
+      {/* Бегущая строка во весь экран */}
+      <div className="scrolling-header">
+        <div className="scrolling-text">
+          The Movie Tracker
+        </div>
+      </div>
+
+      {/* Форма для входа */}
+      <div className="form-container">
+        <input type="text" placeholder="Login" className="input-field" />
+        <input type="password" placeholder="Password" className="input-field" />
+        <button className="login-button">Login</button>
+        <p className="signup-text">
+          You don’t have an account?{' '}
+          <span className="signup-link">SignUp</span>
+        </p>
+      </div>
+
+      {/* Нижняя часть экрана */}
+      <div className="footer">
+        Built with ❤️ GreenField
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
